10d469a4aaac0c9cce42a843b8b3d9effe9a16ed
chiaracomoglio <chiarakom@gmail.com>
2025-01-09 12:07:45 +0100

    Update example.m

--- example.m
+++ example.m
@@ -1,42 +1,48 @@
 %% script: example.m 
 
-% Load Dolphin Social Network
-load('data/data_dolphin.mat')
-
-% Graph
-G = graph(A);
+clear all; close all; format short e; 
 
 %% Algorithm 3
 
-[communities, colors, color_map, num_communities, unique_communities] = algorithm_3(A);
+% Graph: Stanford Bunny
+G.type = 'bunny';
+load data_bunny.mat
+[G.nodes,G.edges,G.A] = GBF_gengraph(G.type);
+
+% Adjacency matrix
+G_plot = graph(G.A);
+
+[communities, colors, color_map, num_communities, unique_communities] = algorithm_3(G.A);
 
 % Visualization of the graph partitioning into communities
 figure;
-h = plot(G, 'NodeCData', communities, 'MarkerSize', 4, 'NodeColor', colors);
-title('Dolphin Social Network - Algorithm 3');
+h = plot(G_plot, 'NodeCData', communities, 'MarkerSize', 3, 'NodeColor', colors, 'XData', G.nodes(:,1), 'YData', G.nodes(:,2));
+title('Stanford Bunny - Algorithm 3');
 h.NodeLabel = {};
+axis equal;
 
 % Node color assignment
  for i = 1:num_communities
      highlight(h, find(communities == unique_communities(i)), 'NodeColor', color_map(i, :));
  end
 
-% Colorbar of community colors
+% Colorbar with community colors
 colormap(color_map);
 c = colorbar;
 c.Ticks = linspace(0, 1, num_communities);
 c.TickLabels = {};
-c.Label.String = 'Comunità';
+c.Label.String = 'Communities';
 
 %% Visualization of the magnitudes of the elements of the leading eigenvector u_1 of B
 
-[communities_mag, colors_mag] = algorithm_1_magnitudes(A);
+[communities_mag, colors_mag] = algorithm_1_magnitudes(G.A);
 
 % Visualization of the graph with the magnitudes of the elements of u_1
 figure;
-h_mag = plot(G, 'NodeCData', communities_mag, 'MarkerSize', 4, 'NodeColor', colors_mag);
-title('Dolphin Social Network - Magnitudes Algorithm 1');
+h_mag = plot(G_plot, 'NodeCData', communities_mag, 'MarkerSize', 3, 'NodeColor', colors_mag, 'XData', G.nodes(:,1), 'YData', G.nodes(:,2));
+title('Stanford Bunny - Magnitudes Algorithm 1');
 h_mag.NodeLabel = {};
+axis equal;
 
 % Color map
 color_map = [
