ce626f2383377ac65720bd61b767eca522052824
Chiara Comoglio <chiarakom@gmail.com>
2025-02-02 15:18:54 +0100

    Update GBF_RLSGBF.m

--- core/GBF_RLSGBF.m
+++ core/GBF_RLSGBF.m
@@ -14,15 +14,15 @@ function [s,c,Kf] = GBF_RLSGBF(bf,idxW,y,lambda)
 % Use:     [s,c,Kf] = GBF_RLSGBF(bf,idxW,y,lambda)
 %
 % Input:
-%          bf     = NxK matrix - the K graph basis vectors
-%          idxW   = K vector - the indices of the K sampling nodes
-%          y      = K vector - the sampling values at the K nodes
-%          lambda = regularization parameter for RLS
+%          bf     : NxK matrix - the K graph basis vectors
+%          idxW   : K vector - the indices of the K sampling nodes
+%          y      : K vector - the sampling values at the K nodes
+%          lambda : regularization parameter for RLS
 %
 % Output:
-%          s     = N-vector - the GBF-RLS solution
-%          c     = K-vector - the coefficients in the basis expansion
-%          Kf    = interpolation matrix of the kernel
+%          s     : N-vector - the GBF-RLS solution
+%          c     : K-vector - the coefficients in the basis expansion
+%          Kf    : interpolation matrix of the kernel
 
 [~,K] = size(bf);
 
